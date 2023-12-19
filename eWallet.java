e83281b744f628cb876362a1193dd179a73b2954
haniffsyahmi <148729374+haniffsyahmi@users.noreply.github.com>
2023-12-19 23:21:27 +0800

    Update eWallet.java

--- eWallet.java
+++ eWallet.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class eWallet {
+    private ArrayList<Transactions> transactions;
+
+    public eWallet() {
+        transactions = new ArrayList<Transactions>();
+    }
+
+    public void addTransaction(Transactions transaction) {
+        transactions.add(transaction);
+    }
 
 }
