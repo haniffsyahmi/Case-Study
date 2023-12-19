c460af972f0f99a9279ea1123f9e5417731e73e0
ezmirnaq <153019971+ezmirnaq@users.noreply.github.com>
2023-12-19 22:30:59 +0800

    Update Transactions.java
    apa2 silakan suruh aku repair

--- Transactions.java
+++ Transactions.java
@@ -1,3 +1,30 @@
 public class Transactions {
-
+class Transaction {
+        private double balance;
+        private double payment;
+        private Date date;
+        private double remainingAmount;
+    
+        public Transaction(double balance, double payment) {
+            this.balance = balance;
+            this.payment = payment;
+            this.date = new Date();
+            this.remainingAmount = balance - payment;
+        }
+    
+        public double getRemainingAmount() {
+            return remainingAmount;
+        }
+    
+        public Date getDate() {
+            return date;
+        }
+  
+        public String toString() {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            return "Transaction on " + dateFormat.format(date) +
+                    ": Balance=" + balance +
+                    ", Payment=" + payment +
+                    ", Remaining Amount=" + remainingAmount;
+        }
 }
