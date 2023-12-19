643106ba4c2223040abdd213932945303c4fe46d
haniffsyahmi <148729374+haniffsyahmi@users.noreply.github.com>
2023-12-19 23:11:03 +0800

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -1,49 +1,54 @@
-
+import java.util.Date;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+public class Transactions{
     private double balance;
     private double payment;
-    private Date date;
+    private Date transactiondate;
     private double remainingAmount;
 
     public Transaction(double balance, double payment) {
         this.balance = balance;
         this.payment = payment;
-        this.date = new Date();
+        this.transactiondate = new Date();
         this.remainingAmount = balance - payment;
     }
 
     public double getBalance() {
         return balance;
     }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
+    
     public double getPayment() {
         return payment;
-    }
-
-    public void setPayment(double payment) {
-        this.payment = payment;
     }
 
     public double getRemainingAmount() {
         return remainingAmount;
     }
 
-    public Date getDate() {
-        return date;
+    public Date gettransactionDate() {
+        return transactiondate;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public double calculateDiscount() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(transactionDate);
+
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if ((hourOfDay >= 12 && hourOfDay <= 14) || (hourOfDay >= 18 && hourOfDay <= 20)) {
+            return 0.1 * balance;
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public String toString() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return "Transaction on " + dateFormat.format(date) +
+        return "Transaction on " + dateFormat.format(transactiondate) +
                 ": Balance=" + balance +
                 ", Payment=" + payment +
                 ", Remaining Amount=" + remainingAmount;
     }
+}
